59e72ea1de5e34f00f1fb9c7bfda123192a5f805
fdnt7 <43757589+fdnt7@users.noreply.github.com.>
2024-02-11 04:42:39 +0700

    Added `#[serde(rename = "...")]` to match the API event type names

--- rhyolite/src/models/track_events.rs
+++ rhyolite/src/models/track_events.rs
@@ -7,25 +7,30 @@ use crate::models::{Severity, Track};
 #[derive(Deserialize, Debug)]
 #[serde(tag = "type")]
 pub enum Event {
+    #[serde(rename = "TrackStartEvent")]
     Start {
         guild_id: Id<GuildMarker>,
         track: Track,
     },
+    #[serde(rename = "TrackEndEvent")]
     End {
         guild_id: Id<GuildMarker>,
         track: Track,
         reason: TrackEndReason,
     },
+    #[serde(rename = "TrackExceptionEvent")]
     Exception {
         guild_id: Id<GuildMarker>,
         track: Track,
         exception: Severity,
     },
+    #[serde(rename = "TrackStuckEvent")]
     Stuck {
         guild_id: Id<GuildMarker>,
         track: Track,
         threshold_ms: i32,
     },
+    #[serde(rename = "WebSocketClosedEvent")]
     WebSocketClosed {
         guild_id: Id<GuildMarker>,
         code: i32,
